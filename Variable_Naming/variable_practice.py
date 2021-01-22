f6d88152bb551dc3659d3080f566b0b3b8c5dd92
Rickcern44 <you@example.com>
2021-01-21 19:46:07 -0500

    Made the variables.py file better.

---
+++ Variable_Naming/variable_practice.py
@@ -0,0 +1,97 @@
+# This file will be to practice your variable naming and manipulation. This is all going to be very simple and is not about the math.
+# This is an excercise to get you comfortable with the naming and typing of variables.
+
+#   1. 4 birds are sitting on a branch. 1 flies away. How many birds are left on
+#     the branch?
+
+# Example
+
+initial_birds = 4
+birds_flew_away = 1
+remaining_birds = initial_birds - birds_flew_away
+
+# You dont have to print it. This is to just get you used to naming things and using variables. You can just to see the outcome if you would like.
+# With this excercise you can just hit the play button up top and it will run it in the terminal below. Or use a seperate window if you want.
+print(remaining_birds)
+
+
+# 2. 3 raccoons are playing in the woods. 2 go home to eat dinner. How
+# many raccoons are left in the woods?
+
+
+# 3. 2 beavers were working on their home. 1 went for a swim. How many
+# beavers are still working on their home?
+
+
+# 4. Mrs. Hilt's favorite first grade classes are baking muffins. Mrs. Brier's
+# class bakes 18 muffins, Mrs. MacAdams's class bakes 20 muffins, and
+# Mrs. Flannery's class bakes 17 muffins. How many muffins does first
+# grade bake in all?
+
+
+# 5. (Remember use float for money) Johnny has $20.00 dollars when he leaves the house. He stops and grabs a newspaper which is $2.23.
+# He then stops at the gas station and grabs a roller griller (The gross steak one that has sub human grade steak in it) which is $2.50.
+# Before he gets home he grabs a drink from mcdonalds which is $1.33. How much does Jhonny have when he is done.
+
+
+# 6. Josh had 16 marbles in his collection. He lost 7 marbles. How many
+# marbles does he have now?
+
+
+# (HINT this will use booleans and compare them to one another to compare bools you will be using the == operator).
+# Willy is taking a lie detector and he needs to not lie in order to pass the test. He lies on the first question and answers the second
+# truthfully. Does Willy pass the test? ***The answer will be a single bool.***
+
+
+# 8. Mrs. Hilt spent 1 hour and 30 minutes shoveling snow on Saturday
+# morning and 45 minutes shoveling snow on Saturday afternoon. How
+# much total time did she spend shoveling snow?
+
+
+# 9. Mrs. Hilt bought 6 hot dogs. Each hot dog cost 50 cents. How much
+# money did she pay for all of the hot dogs?
+
+
+# 10. Mrs. Hilt has 50 cents. A pencil costs 7 cents. How many pencils can
+# she buy with the money she has?
+
+
+# 12. Kim has 4 cousins. She wants to give each one 5 pieces of gum. How
+# much gum will she need?
+
+
+# 13. There are 10 stickers on a page. If you have 22 pages of stickers, how
+# many stickers do you have?
+
+
+# 14. Susie’s mother collected all family pictures and wanted to place all of
+# them in an album. If an album can contain 20 pictures, how many
+# albums will she need if there are 480 pictures?
+
+
+# 15. Joe, Susie’s brother, collected all 94 trading cards scattered in his
+# room and placed them in boxes. If a full box can hold a maximum of 8
+# cards, how many boxes were filled and how many cards are there in
+# the unfilled box?
+
+
+# ***Challenge Problems***
+# You may have to do some google driven development to figure these out.
+
+# Bill and Jill are house painters. Bill can paint a 12 x 14 room in 2.15 hours, while Jill averages
+# 1.90 hours. How long will it take the two painter working together to paint 5 12 x 14 rooms?
+# Hint: Calculate the hourly rate for each painter, combine them, and then divide the total walls in feet by the combined hourly rate of the painters.
+# Challenge: How many days will it take the pair to paint 623 rooms assuming they work 8 hours a day?.
+
+
+# Create and assign variables to hold your first name, last name, and middle initial. Using concatenation,
+# build an additional variable to hold your full name in the order of last name, first name, middle initial. The
+# last and first names should be separated by a comma followed by a space, and the middle initial must end
+# with a period.
+# Example: "Hopper, Grace B."
+
+
+# The distance between New York and Chicago is 800 miles, and the train has already travelled 537 miles.
+# What percentage of the trip has been completed?
+# Hint: The percent completed is the miles already travelled divided by the total miles.
+# Challenge: Display as an integer value between 0 and 100 using casts.
